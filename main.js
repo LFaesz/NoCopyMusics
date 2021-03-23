4d23a28d27ce56a9999b03180b5607a18d0d2912
Lucas Faes <lucasfaes2010@gmail.com>
2021-03-23 12:30:58 -0300

    Atualizaçao login page

--- main.js
+++ main.js
@@ -2,7 +2,46 @@ function playAudio(url) {
     new Audio(url).play();
   }
 
-let play = function(){
-    document.getElementById("audio").play()
+  function playAudio(url) {
+    new Audio(url).play1();
+  }
+
+  function temaa(){
+    var temaa = document.getElementById('container')
+    var logo = document.getElementById('logo')
+    var link1 = document.getElementById('link1')
+    var link2 = document.getElementById('link2')
+    var link3 = document.getElementById('link3')
+    var link4 = document.getElementById('link4')
+    var link5 = document.getElementById('link5')
+    var link6 = document.getElementById('link6')
+    temaa.style.backgroundColor = 'black'
+    logo.style.color = 'white'
+    link1.style.color = 'white'
+    link2.style.color = 'white'
+    link3.style.color = 'white'
+    link4.style.color = 'white'
+    link5.style.color = 'white'
+    link6.style.color = 'white'
+    
 }
 
+function tema(){
+    var tema = document.getElementById('container')
+    var logo = document.getElementById('logo')
+    var link1 = document.getElementById('link1')
+    var link2 = document.getElementById('link2')
+    var link3 = document.getElementById('link3')
+    var link4 = document.getElementById('link4')
+    var link5 = document.getElementById('link5')
+    var link6 = document.getElementById('link6')
+    tema.style.backgroundColor = 'white'
+    logo.style.color = 'black'
+    link1.style.color = 'black'
+    link2.style.color = 'black'
+    link3.style.color = 'black'
+    link4.style.color = 'black'
+    link5.style.color = 'black'
+    link6.style.color = 'black'
+
+}
