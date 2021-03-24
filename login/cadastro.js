5f4413ed691c856989f2274f20bfd50537b75eea
Lucas Faes <lucasfaes2010@gmail.com>
2021-03-24 16:15:39 -0300

    Update tema cadastro

---
+++ login/cadastro.js
@@ -0,0 +1,66 @@
+function temaa(){
+    var terms = document.getElementById('terms')
+    var log = document.getElementById('4login')
+    var ps = document.getElementById('ps')
+    var title = document.getElementById('title')
+    var sec = document.getElementById('sec')
+    var fundo = document.body
+    var temaa = document.getElementById('container')
+    var logo = document.getElementById('logo')
+    var link1 = document.getElementById('link1')
+    var link2 = document.getElementById('link2')
+    var link3 = document.getElementById('link3')
+    var link4 = document.getElementById('link4')
+    var link5 = document.getElementById('link5')
+    var link6 = document.getElementById('link6')
+    var link7 = document.getElementById('link7')
+    terms.style.color = 'black'
+    log.style.color = 'black'
+    ps.style.color = 'black'
+    title.style.color = 'black'
+    sec.style.backgroundColor = 'rgb(192, 189, 189)'
+    fundo.style.backgroundColor = 'black'
+    temaa.style.backgroundColor = 'black'
+    logo.style.color = '#AAA'
+    link1.style.color = '#AAA'
+    link2.style.color = '#AAA'
+    link3.style.color = '#AAA'
+    link4.style.color = '#AAA'
+    link5.style.color = '#AAA'
+    link6.style.color = '#AAA'
+    link7.style.color = '#AAA'
+}
+
+function tema(){ //  BRANCO
+    var terms = document.getElementById('terms')
+    var log = document.getElementById('4login')
+    var ps = document.getElementById('ps')
+    var title = document.getElementById('title')
+    var sec = document.getElementById('sec')
+    var fundo = document.body
+    var tema = document.getElementById('container')
+    var logo = document.getElementById('logo')
+    var link1 = document.getElementById('link1')
+    var link2 = document.getElementById('link2')
+    var link3 = document.getElementById('link3')
+    var link4 = document.getElementById('link4')
+    var link5 = document.getElementById('link5')
+    var link6 = document.getElementById('link6')
+    var link7 = document.getElementById('link7')
+    log.style.color = 'white'
+    terms.style.color = 'white'
+    ps.style.color = 'white'
+    title.style.color = 'white'
+    sec.style.backgroundColor = 'black'
+    tema.style.backgroundColor = 'white'
+    fundo.style.backgroundColor = 'rgb(236, 236, 236)'
+    logo.style.color = 'black'
+    link1.style.color = 'black'
+    link2.style.color = 'black'
+    link3.style.color = 'black'
+    link4.style.color = 'black'
+    link5.style.color = 'black'
+    link6.style.color = 'black'
+    link7.style.color = 'black'
+}
+
